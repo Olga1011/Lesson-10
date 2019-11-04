c7031551a75cb75edad2fd2695d69bc1559f3cf6
Olga1011 <olgacovali1011@gmail.com>
2019-11-04 15:49:56 +0200

    SU010 Homework lesson-10

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('myCurrentAge');
+var myCurrentAge = 29;
+if (myCurrentAge >= 25) {
+ console.log('I am over 25 years old');
+} else {
+    console.log('I am under 25');
+}
+
+var result = ( myCurrentAge >= 25 )? 'I am over 25 years old' : 'I am under 25 yars old';
